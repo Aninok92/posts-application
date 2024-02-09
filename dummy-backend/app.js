88e197f6b2a6647192197d3b6dffc13025b0959d
Nina Vasilieva <aninok92@gmail.com>
2024-02-09 12:15:55 +0200

    add dotenv

--- dummy-backend/app.js
+++ dummy-backend/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,14 +13,15 @@ app.use((req, res, next) => {
   // Attach CORS headers
   // Required when using a detached backend (that runs on a different domain)
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
+const PORT = process.env.PORT;
+
 app.get('/posts', async (req, res) => {
   const storedPosts = await getStoredPosts();
-  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
   res.json({ posts: storedPosts });
 });
 
@@ -40,4 +43,14 @@ app.post('/posts', async (req, res) => {
   res.status(201).json({ message: 'Stored new post.', post: newPost });
 });
 
-app.listen(8080);
+app.delete('/posts/:id', async (req, res) => {
+  const postIdToDelete = req.params.id;
+  const existingPosts = await getStoredPosts();
+  const updatedPosts = existingPosts.filter(post => post.id !== postIdToDelete);
+  await storePosts(updatedPosts);
+  res.status(200).json({ message: 'Post deleted successfully', posts: updatedPosts });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
